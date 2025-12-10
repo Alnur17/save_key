e7724463d0de57352790c9c32c6796b889a9bc0e
anisur-rahman-samim <samimcse7@gmail.com>
2025-12-10 12:13:52 +0600

    solve all

--- lib/app/modules/local_business/deals/views/create_deals_view.dart
+++ lib/app/modules/local_business/deals/views/create_deals_view.dart
@@ -78,15 +78,6 @@ class CreateDealsView extends StatelessWidget {
             ),
             sh12,
             Text(
-              'Price',
-              style: h4,
-            ),
-            sh8,
-            CustomTextField(
-              hintText: 'Enter price',
-            ),
-            sh12,
-            Text(
               'Discount Type',
               style: h4,
             ),
@@ -115,48 +106,6 @@ class CreateDealsView extends StatelessWidget {
                 ],
               ),
             ),
-            // sh12,
-            // Text(
-            //   'Eligibility/Frequency',
-            //   style: h4,
-            // ),
-            // sh8,
-            // Obx(
-            //   () => ReusableDropdown(
-            //     options: [
-            //       'Once per month',
-            //       'Once per week',
-            //       'One-Time Only',
-            //       'Unlimited'
-            //     ],
-            //     hintText: 'Select a type',
-            //     selectedValue: controller.selectedEligibility.value,
-            //     onChanged: (value) => controller.changeEligibility(value),
-            //   ),
-            // ),
-            // sh12,
-            // Text(
-            //   'Schedule',
-            //   style: h4,
-            // ),
-            // sh8,
-            // Obx(
-            //   () => ReusableDropdown(
-            //     options: ['Always-on', 'Happy Hours', 'Promo Window'],
-            //     hintText: 'Select a type',
-            //     selectedValue: controller.selectedSchedule.value,
-            //     onChanged: (value) => controller.changeSchedule(value),
-            //   ),
-            // ),
-            // sh12,
-            // Text(
-            //   'Create Private Code',
-            //   style: h4,
-            // ),
-            // sh8,
-            // CustomTextField(
-            //   hintText: 'D-VG326',
-            // ),
             sh12,
             Text(
               'Location',
@@ -174,15 +123,8 @@ class CreateDealsView extends StatelessWidget {
             sh8,
             CustomTextField(
               hintText: '5 times',
-            ),sh12,
-            Text(
-              'Custom Discount Limitation',
-              style: h4,
             ),
-            sh8,
-            CustomTextField(
-              hintText: '5 times',
-            ),
+
             sh60,
           ],
         ),
--- lib/app/modules/member/home/views/home_view.dart
+++ lib/app/modules/member/home/views/home_view.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'package:get/get.dart';
 import 'package:save_key/app/modules/member/home/views/discount_details_view.dart';
 import 'package:save_key/app/modules/member/home/views/my_search_view.dart';
 import 'package:save_key/app/modules/member/home/views/notifications_view.dart';
-import 'package:save_key/app/modules/member/home/views/qr_code_view.dart';
 import 'package:save_key/app/modules/member/home/views/verification_membership_view.dart';
+import 'package:save_key/common/helper/custom_profile_image.dart';
 import 'package:save_key/common/size_box/custom_sizebox.dart';
-import 'package:save_key/common/widgets/custom_textfield.dart';
-
 import '../../../../../common/app_color/app_colors.dart';
 import '../../../../../common/app_images/app_images.dart';
 import '../../../../../common/app_text_style/styles.dart';
-import '../../../../../common/helper/custom_filter_chip.dart';
-import '../../../../../common/helper/custom_profile_image.dart';
-import '../../../../../common/helper/deal_card.dart';
 import '../../../../../common/helper/discount_card.dart';
-import '../../../../../common/helper/filter_dialog.dart';
+
 import '../controllers/home_controller.dart';
 
 class HomeView extends StatefulWidget {
@@ -27,8 +21,22 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
+class _HomeViewState extends State<HomeView> with SingleTickerProviderStateMixin {
   final HomeController homeController = Get.put(HomeController());
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,43 +64,24 @@ class _HomeViewState extends State<HomeView> {
                       TextSpan(text: 'Hello! '),
                       TextSpan(
                         text: 'Sultan',
-                        style: h3.copyWith(
-                          color: AppColors.greenNormal,
-                        ),
+                        style: h3.copyWith(color: AppColors.greenNormal),
                       ),
                     ],
                   ),
                 ),
                 sh5,
-                Text(
-                  'MohaKhali, Dhaka',
-                  style: h5,
-                ),
+                Text('MohaKhali, Dhaka', style: h5),
               ],
             ),
             Spacer(),
             GestureDetector(
-              onTap: () {
-                Get.to(() => MySearchView());
-              },
-              child: Image.asset(
-                AppImages.searchCircle,
-                scale: 4,
-              ),
+              onTap: () => Get.to(() => MySearchView()),
+              child: Image.asset(AppImages.searchCircle, scale: 4),
             ),
             sw8,
             GestureDetector(
-              onTap: () {
-                Get.to(() => NotificationsView());
-                // showDialog(
-                //   context: context,
-                //   builder: (context) => const FilterDialog(),
-                // );
-              },
-              child: Image.asset(
-                AppImages.notificationCircle,
-                scale: 4,
-              ),
+              onTap: () => Get.to(() => NotificationsView()),
+              child: Image.asset(AppImages.notificationCircle, scale: 4),
             )
           ],
         ),
@@ -100,23 +89,19 @@ class _HomeViewState extends State<HomeView> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          // Membership Active Banner
           Padding(
             padding: const EdgeInsets.only(left: 20, right: 20, bottom: 12).r,
             child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 8.h),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: AppColors.greenLight,
-                border: Border.all(
-                  color: AppColors.greenNormal,
-                ),
+                borderRadius: BorderRadius.circular(12.r),
+                color: AppColors.greenLight.withOpacity(0.2),
+                border: Border.all(color: AppColors.greenNormal.withOpacity(0.3)),
               ),
               child: Row(
                 children: [
-                  Image.asset(
-                    AppImages.checkMark,
-                    scale: 4,
-                  ),
+                  Image.asset(AppImages.checkMark, scale: 4),
                   sw12,
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -130,61 +115,82 @@ class _HomeViewState extends State<HomeView> {
               ),
             ),
           ),
+
+          // Title
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20).r,
-            child: Text(
-              'Available Discounts',
-              style: h3,
-            ),
+            child: Text('Available Discounts', style: h3),
           ),
           sh12,
 
-          // SizedBox(
-          //   height: 35.h,
-          //   child: ListView.builder(
-          //     scrollDirection: Axis.horizontal,
-          //     padding: EdgeInsets.symmetric(horizontal: 20.w),
-          //     itemCount: homeController.categories.length,
-          //     itemBuilder: (context, index) {
-          //       final category = homeController.categories[index];
-          //       return Obx(() => CustomFilterChip(
-          //             text: category,
-          //             isSelected:
-          //                 homeController.selectedCategory.value == category,
-          //             onTap: () => homeController.selectCategory(category),
-          //           ));
-          //     },
-          //   ),
-          // ),
-          // sh20,
-          Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.symmetric(horizontal: 20).r,
-              itemCount: 5,
-              itemBuilder: (context, index) => Padding(
-                padding: EdgeInsets.only(
-                  bottom: index == 5 - 1 ? 20 : 8,
-                ),
-                child: DiscountCard(
-                  image: "https://picsum.photos/200",
-                  title: "Cappuccino Coffee",
-                  offer: "20% off on all time",
-                  usage: "2/5",
-                  infoTitle: "Location",
-                  infoValue: "Mohakhali, Dhaka",
-                  isBookmarked: false,
-                  onButtonTap: () {
-                    Get.to(VerificationMembershipView());
-                  },
-                  onCardTap: () {
-                    Get.to(DiscountDetailsView());
-                  },
-                ),
+          // TabBar
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20.w),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: AppColors.grey.withOpacity(0.3), width: 1),
               ),
+            ),
+            child: TabBar(
+              controller: _tabController,
+              labelColor: AppColors.greenNormal,
+              unselectedLabelColor: AppColors.grey,
+              labelStyle: h4.copyWith(fontWeight: FontWeight.w600),
+              unselectedLabelStyle: h4,
+              indicatorColor: AppColors.greenNormal,
+              indicatorWeight: 3,
+              tabs: [
+                Tab(text: 'In-Person'),
+                Tab(text: 'Online'),
+              ],
+            ),
+          ),
+          sh16,
+
+          // TabBarView
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                // In-Person Tab
+                _buildDiscountList(isOnline: false),
+                // Online Tab
+                _buildDiscountList(isOnline: true),
+              ],
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildDiscountList({required bool isOnline}) {
+    return ListView.builder(
+      padding: EdgeInsets.symmetric(horizontal: 20.w),
+      itemCount: 6, // You can change this based on data
+      itemBuilder: (context, index) {
+        final String infoTitle = isOnline ? "Website" : "Location";
+        final String infoValue = isOnline ? "www.example${index + 1}.com" : "Mohakhali, Dhaka";
+
+        return Padding(
+          padding: EdgeInsets.only(bottom: index == 5 ? 20.h : 8.h),
+          child: DiscountCard(
+            image: "https://picsum.photos/seed/$index/300/200",
+            title: isOnline ? "Online Deal ${index + 1}" : "Cappuccino Coffee",
+            offer: "${15 + index * 5}% off",
+            usage: "${index + 1}/5",
+            infoTitle: infoTitle,
+            infoValue: infoValue,
+            isBookmarked: index.isEven,
+            onButtonTap: () {
+              Get.to(() => VerificationMembershipView());
+            },
+            onCardTap: () {
+              Get.to(() => DiscountDetailsView());
+            },
+          ),
+        );
+      },
     );
   }
 }
